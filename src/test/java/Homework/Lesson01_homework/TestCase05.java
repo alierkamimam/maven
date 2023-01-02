007c516ad1a8a748e6df4b7268d934bd4bc1d8d0
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2023-01-02 12:38:06 +0300

    added

---
+++ src/test/java/Homework/Lesson01_homework/TestCase05.java
@@ -0,0 +1,35 @@
+package Homework.Lesson01_homework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TestCase05 {
+    public static void main(String[] args) throws InterruptedException {
+
+        //TestCase05:Checking 'Check All' button
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2fTestComplete12%2fWebOrders%2fDefault.aspx");
+        //Valid username
+        driver.findElement(By.xpath("//input[@id=\"ctl00_MainContent_username\"]")).sendKeys("Tester");
+
+        //Invalid username
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
+
+        //click the login button
+        driver.findElement(By.xpath("//input[@id='ctl00_MainContent_login_button']")).click();
+
+
+        //click the 'check all' button
+        driver.findElement(By.id("ctl00_MainContent_btnCheckAll")).click();
+        Thread.sleep(5000);
+
+        driver.quit();
+    }
+
+}
