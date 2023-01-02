c8800e9f8b047bb07293dd6f50173f2cada1720f
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2023-01-02 12:34:04 +0300

    added

---
+++ src/test/java/Lessons/SecondLesson/section10/ActionDemo.java
@@ -0,0 +1,34 @@
+package Lessons.SecondLesson.section10;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
+
+public class ActionDemo {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.manage().window().maximize();
+
+
+        Actions a = new Actions(driver);
+        WebElement move=driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
+
+        a.moveToElement(driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"))).click()
+                .keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+
+        //Moves to Specific  Element
+        a.moveToElement(move).contextClick().build().perform();
+
+
+
+    }
+}
