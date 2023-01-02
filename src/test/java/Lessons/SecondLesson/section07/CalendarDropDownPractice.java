e25065cc6ebc250a99bacc76489568878fa04bbc
ALİ ERKAM İMAM <imam.ali.erkam@gmail.com>
2023-01-02 12:37:11 +0300

    added

---
+++ src/test/java/Lessons/SecondLesson/section07/CalendarDropDownPractice.java
@@ -0,0 +1,28 @@
+package Lessons.SecondLesson.section07;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class CalendarDropDownPractice {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        //opening url
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+        System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
+        driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+        System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
+
+        if ((driver.findElement(By.id("Div1")).getAttribute("style").contains("1"))){
+            System.out.println("its enabled");
+            Assert.assertTrue(true);
+        }else
+            Assert.assertTrue(false);
+
+
+
+    }
+}
